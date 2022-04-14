0361e4329ba040069da77a17d9018d978d6a0b4d
Thiago <thiagowirzdemoura@gmail.com>
2022-04-14 19:57:35 -0300

    Inital Commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
