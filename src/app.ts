511287db19c98056cdcca9499f27d4c0074607e4
Thiago <thiagowirzdemoura@gmail.com>
2022-04-14 22:10:11 -0300

    feat: error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import router from "./routers/index.js";
+import {errorHandler} from "./middlewares/errorHandlerMiddleware.js"
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(router)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000;
--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import * as cardRepository from "../repositories/cardRepository.js";
 
 export async function createCard(req: Request, res: Response) {
-
+  const {employedId, type} = req.body;
+  const apiKey = req.headers["x-api-key"].toString()
 }
 
