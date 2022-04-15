9b75af4a9a8462bc7de547a3027e74f048b6ff8a
Thiago <thiagowirzdemoura@gmail.com>
2022-04-14 22:21:40 -0300

    feat: error employee not found by id

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,8 +1,13 @@
 import * as companyRepository from "../repositories/companyRepository.js"
+import * as employeeRepository from "../repositories/employeeRepository.js"
 export async function createCard(employeeId: number, type: string, apiKey: string) {
   
   const existCompany = await companyRepository.findByApiKey(apiKey)
   if(!existCompany){
     throw {type: "not_found", message: "Company not found"}
+  }
+  const existEmployee = await employeeRepository.findById(employeeId)
+  if(!existEmployee){
+    throw {type: "not_found", message: "Employee not found"}
   }
 }
