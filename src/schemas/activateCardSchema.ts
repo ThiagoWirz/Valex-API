beafed8d0ece574381e39e29a0157a1244bd962c
iagod <iago.tostes@cubos.io>
2022-04-27 17:21:32 -0300

    Preparando revisão de código

--- src/schemas/activateCardSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-const activateCardSchema = joi.object({
-  securityCode: joi.string(),
-  password: joi.string().pattern(/\d{4}/)
-})
-
-export default activateCardSchema
--- src/utils/balanceUtils.ts
+++
@@ -1,9 +0,0 @@
-export function calculateBalance(recharges:any[], transactions:any[]){
-
-  let sumOfRecharges = 0
-  let sumOfTransactions = 0
-  recharges.forEach(recharge => sumOfRecharges += recharge.amount)
-  transactions.forEach(transaction => sumOfTransactions += transaction.amount)
- 
-  return sumOfRecharges - sumOfTransactions
-}
